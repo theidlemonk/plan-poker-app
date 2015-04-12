b69b1d9657351c6dbd6637740f6dff39fd4b1bf5
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-12 15:13:45 -0400

    plan poker initial commit, add tabs decks and about

---
+++ www/js/app.js
@@ -0,0 +1,53 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('plan-poker', ['ionic','plan-poker.controllers'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html',
+    controller: 'TabsCtrl'
+  })
+
+  .state('tab.decks', {
+    url: '/decks',
+    views: {
+      'tab-decks': {
+        templateUrl: 'templates/decks.html',
+        controller: 'DecksCtrl'
+      }
+    }
+  })
+
+  .state('tab.about', {
+    url: '/about',
+    views: {
+      'tab-about': {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutCtrl'
+      }
+    }
+  })
+
+  $urlRouterProvider.otherwise('/tab/decks');
+
+})
---
+++ www/js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('plan-poker.controllers', ['ionic', 'plan-poker.services'])
+
+.controller('TabsCtrl', function($scope){
+  
+})
+
+
+.controller('DecksCtrl', function($scope){
+
+  $scope.decks = [
+  {
+    "name":"Fibonacci"
+  },
+  {
+    "name":"Tshirt Sizes"
+  }
+  ]
+  
+})
+
+// .controller('DeckDetailCtrl', function($scope){
+  
+// });
+
+.controller('AboutCtrl', function($scope){
+  
+})
